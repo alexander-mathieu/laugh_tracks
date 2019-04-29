94cc0229346afae74c18f5082d5aa6ae3f0277bc
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-29 17:48:51 -0600

    Add coverage directory to .gitignore, fix comedian spec

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Comedian type: :model do
+describe Comedian, type: :model do
   describe "validations" do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:age)}
