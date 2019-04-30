96a1243339ed1438c5b399c4e530af68c3926317
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-30 16:25:49 -0600

    Reorganize special/comedian spec

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 describe Comedian, type: :model do
+
+  describe "relationships" do
+    it {should have_many(:specials)}
+  end
+
   describe "validations" do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:age)}
     it {should validate_presence_of(:birthplace)}
     it {should validate_presence_of(:image_url)}
-  end
-
-  describe "relationships" do
-    it {should have_many(:specials)}
   end
 
 end
--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 describe Special, type: :model do
+
+  describe "relationships" do
+    it {should belong_to(:comedian)}
+  end
+
   describe "validations" do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:runtime_mins)}
     it {should validate_presence_of(:comedian_id)}
     it {should validate_presence_of(:image_url)}
   end
-
-  describe "relationships" do
-    it {should belong_to(:comedian)}
-  end
-
 
 end
