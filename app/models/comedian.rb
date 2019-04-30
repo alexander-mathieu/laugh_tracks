ea03f465c4c50731ab8d66afe9b2048344e8fbd7
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-30 16:23:02 -0600

    Add comedian/special relationship in comedian model, update spec

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,4 +1,5 @@
 class Comedian < ApplicationRecord
+has_many :specials
 
   validates_presence_of :name, :age, :birthplace, :image_url
 
--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -7,4 +7,9 @@ describe Comedian, type: :model do
     it {should validate_presence_of(:birthplace)}
     it {should validate_presence_of(:image_url)}
   end
+
+  describe "relationships" do
+    it {should have_many(:specials)}
+  end
+
 end
