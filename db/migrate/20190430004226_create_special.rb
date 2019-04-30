19b97ba408e4b25ff4b3f35b205665726e7f1eee
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-30 15:09:39 -0600

    Update special migration to include image url and timestamps

--- db/migrate/20190430004226_create_special.rb
+++ db/migrate/20190430004226_create_special.rb
@@ -1,9 +1,12 @@
 class CreateSpecial < ActiveRecord::Migration[5.1]
   def change
     create_table :specials do |t|
-      t.string :name
-      t.time :runtime
+      t.string  :name
+      t.integer :runtime_mins
       t.integer :comedian_id
+      t.string  :image_url
+
+      t.timestamps
     end
   end
 end
