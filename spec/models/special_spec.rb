e454012d821405770053959aafc198f1d956f7e5
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-04-30 15:19:47 -0600

    Add validations to special model and spec

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe Special, type: :model do
   describe "validations" do
     it {should validate_presence_of(:name)}
-    it {should validate_presence_of(:runtime)}
+    it {should validate_presence_of(:runtime_mins)}
     it {should validate_presence_of(:comedian_id)}
+    it {should validate_presence_of(:image_url)}
   end
 end
