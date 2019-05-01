d469a739e51c237e9029e8b0b784f7780e1f93c7
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-01 15:19:48 -0600

    Remove image url validations from special model and spec

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -10,7 +10,6 @@ describe Special, type: :model do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:runtime_mins)}
     it {should validate_presence_of(:comedian_id)}
-    it {should validate_presence_of(:image_url)}
   end
 
 end
