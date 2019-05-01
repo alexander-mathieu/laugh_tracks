37e125df4e145f1e50f025a2d6a9b04cd1581d95
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-01 15:32:29 -0600

    Change arguments for specials in user sees comedian specials spec

--- spec/features/comedians/user_sees_comedian_specials_spec.rb
+++ spec/features/comedians/user_sees_comedian_specials_spec.rb
@@ -6,13 +6,13 @@ describe "as a user on the comedian index" do
       comedian_1 = Comedian.create(name: "Kevin Hart", age: 39, birthplace: "Philadelphia, PA", image_url: "http://www.philly.com/resizer/bHw4bvIexfBozzYo6gSgV1vpEXk=/1400x932/center/middle/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/DBTWGCGWV5C7ZJBACNCBPOCUWE.jpg")
       comedian_2 = Comedian.create(name: "Dave Chappelle", age: 45, birthplace: "Washington, D.C.", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTII3JGyHZiZx-tRsKYcDk8jotJ6kK6yGNiCof1mLfTw5DxekFZtg")
 
-      special_1a = comedian_1.specials.create(name: "Live in NY!", runtime_mins: 60)
-      special_2a = comedian_1.specials.create(name: "Live in Boston!", runtime_mins: 70)
-      special_3a = comedian_1.specials.create(name: "Live in Chitown!", runtime_mins: 80)
+      special_1a = comedian_1.specials.create(name: "I'm a Grown Little Man", runtime_mins: 72)
+      special_2a = comedian_1.specials.create(name: "Laugh at My Pain", runtime_mins: 89)
+      special_3a = comedian_1.specials.create(name: "Let Me Explain", runtime_mins: 75)
 
-      special_1b = comedian_2.specials.create(name: "Live in Pelham, AL!", runtime_mins: 60)
-      special_2b = comedian_2.specials.create(name: "Live in Pelham, NH!", runtime_mins: 70)
-      special_3b = comedian_2.specials.create(name: "Live in Pelham, GA!", runtime_mins: 80)
+      special_1b = comedian_2.specials.create(name: "Killin' Them Softly", runtime_mins: 57)
+      special_2b = comedian_2.specials.create(name: "For What It's Worth", runtime_mins: 60)
+      special_3b = comedian_2.specials.create(name: "Equanimity", runtime_mins: 60)
 
       visit '/comedians'
 
