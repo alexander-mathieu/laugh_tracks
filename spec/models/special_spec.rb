4a749cdfad384bcb050e429d3ef4cc519a6bc1af
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-01 17:56:05 -0600

    Seed development database

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -9,7 +9,6 @@ describe Special, type: :model do
   describe "validations" do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:runtime_mins)}
-    it {should validate_presence_of(:comedian_id)}
   end
 
 end
